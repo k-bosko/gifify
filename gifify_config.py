d8235235ce200f05b5eb966decc16ce6c15203e1
Katerina Bosko <katerinabosko@gmail.com>
2022-05-02 13:39:17 -0700

    created new gifify config

---
+++ gifify_config.py
@@ -0,0 +1,6 @@
+USER_TABLE = 'Gifify-users'
+UPLOAD_BUCKET = 'gifify-src-kbosko'
+DOWNLOAD_BUCKET = 'gifify-out-kbosko'
+REGION = 'us-east-1'
+
+ALLOWED_EXTENSIONS = set(['mov', 'mp4', 'wmv', 'avi', 'flv', 'm4p', 'mpg', 'mpeg', 'mpv', '3gp', '3g2', 'webm'])
