86b4e4a3096a0ba108486be41b357b3d1c45c657
Katerina Bosko <katerinabosko@gmail.com>
2022-05-02 14:50:56 -0700

    updated gifify_config

--- gifify_config.py
+++ gifify_config.py
@@ -1,13 +1,12 @@
 access_credentials = {
-    "aws_access_key_id": "ASIAXWSOBH3NTOQX2MM7",
-    "aws_secret_access_key": "Fyhv7gDO0yBD8YkiNvA1cwgZTvsva7AM3qBOn8Xd",
-    "aws_session_token": "FwoGZXIvYXdzEGgaDMxh10mPySyCXLi2KyLJAaWNCFc+UDF6uxbf+R0TvV4VlKiEk1dW3U+bt1YPgdEt+fYUkwI4NE+tWZGsudTXIRYmjCTJRgAvbMZ3xKSA0H4A1+om6N7g9kp0XqmfPHHSa0XprTqSOREIGHDJNyH38tRrzK5I+WnwSGZM0JGXkgBhQs8K4QE5HdhT8vHCpgo78Nyc25smI9WUCSKxu44kfMVrm9u5lU5JU8kqzy1jDMEkABIErEpXLSgo27jwdDDtvf64Mo2cLG9//iEmVFS/jW42XmnvQxKDFCj3/LaTBjIt1syhYpq4WkXBXldnM+sM6qorS99x2W+OMAYNBVdc3okhHzdlLJlkv5wtnDTe",
+    "aws_access_key_id": "",
+    "aws_secret_access_key": "",
+    "aws_session_token": "",
     "region_name": "us-east-1"
 }
 
 USER_TABLE = 'Gifify-users'
 UPLOAD_BUCKET = 'gifify-src-kbosko'
 DOWNLOAD_BUCKET = 'gifify-out-kbosko'
-REGION = 'us-east-1'
 
 ALLOWED_EXTENSIONS = set(['mov', 'mp4', 'wmv', 'avi', 'flv', 'm4p', 'mpg', 'mpeg', 'mpv', '3gp', '3g2', 'webm'])
