8231aff6d75686d1786f4a5677a5a02887038d92
Katerina Bosko <katerinabosko@gmail.com>
2023-02-27 15:36:18 -0600

    updated config file

--- gifify_config.py
+++ gifify_config.py
@@ -1,7 +1,7 @@
 access_credentials = {
-    "aws_access_key_id": "ASIAXWSOBH3NQ5ZP5LHX",
-    "aws_secret_access_key": "vIR06s+j590de86ABlpjll5X0yQ77fxs930hgQtW",
-    "aws_session_token": "FwoGZXIvYXdzEJj//////////wEaDObb5TR027s4C7rLWCLJAZVOXVSsm/ZkrZRwnpITjtXn/9ExKU/QC8yiUicQ3wuJE/IGJT0kM5Ukw2Ab6JDtObhy4GJIFt+WStbKeBqz6H6230zAF9ITBTVo89CXYhu1qZn0fSG4o5MTvxWic5uxbPUTkfBqP++JqOodKmyYJZrW8Pp0Bca821WfypyagLVNjJhvdLHQRWJpARpwA6MiLGx4P5bqfl1EzFU96VaPM0bIdduKhD7IpRC8aQkmsn4P4oMiCCBL+7uvpG9IvRGdd5Uz57Fn6bWOeyjZtMGTBjItnQXhAf1TEXVoNyaf1aCVxRMHBKlAMAUxOs3HKzr33S49St5Lx6ZUQPk7boe/",
+    "aws_access_key_id": "<insert-your-own>",
+    "aws_secret_access_key": "<insert-your-own>",
+    "aws_session_token": "<insert-your-own>",
     "region_name": "us-east-1"
 }
 
